5a164804a784575f8f614c1fe6f4a403d7dc5df9
CharlieJP05 <charliejparker05@gmail.com>
2026-03-28 21:06:43 +0000

    note for mapping

--- MicroMouse/Core/Src/Mapping.c
+++ MicroMouse/Core/Src/Mapping.c
@@ -4,9 +4,11 @@
 
 void Mapping_init()
 {
-    
+
 }
 // possible functions:
 
 // get all distances
 // ???
+
+// notes: US sensor accurtate up to 60 but maybe max it at 50?. any more and disregard. below 3 it goes back up so? idk
