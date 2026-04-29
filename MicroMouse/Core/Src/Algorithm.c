2572953beae3f2314c95e09b6bfea226b261e1b8
Charlie Parker <charliejparker05@gmail.com>
2026-04-29 16:39:39 +0100

    added comments to algo

--- MicroMouse/Core/Src/Algorithm.c
+++ MicroMouse/Core/Src/Algorithm.c
@@ -5,72 +5,65 @@
 #include <stdio.h>
 #include "queue.h"
 
-// remember: add new funcs to h, any inputs are needed there too.
-
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-
-
-int flood[8][6]; // rows then columns this is the full map
+int flood[map_w][map_h]; // flood variable stores the floodfill values for the whole map
 
 #define N 0b00000011
 #define E 0b00001100
 #define S 0b00110000
-#define W 0b11000000
+#define W 0b11000000 // bit masks for each direction selection
 
-uint8_t direction[4] = {N, E, S, W};
+uint8_t direction[4] = {N, E, S, W}; // list of all directions for lookup
 
-int dir_lookup[4][2]  = {{0,1},{1,0},{0,-1},{-1,0}};
+int dir_lookup[4][2]  = {{0,1},{1,0},{0,-1},{-1,0}}; // lookup of the deltas of directions
 
 
-void flood_fill_calc(uint8_t map[map_w][map_h], int goal[2]){
-	// initialise flood map
+void flood_fill_calc(uint8_t map[map_w][map_h], int goal[2]){ // calculates the floodfill
+	// initialise flood map to -1 all over
 	for(int r = 0; r < map_w; r++){
 		for(int c = 0; c < map_h; c++){
 			flood[r][c] = -1;
 		}
 	}
 
-	// set goal
-	//flood[goal[0]][goal[1]] = 0;
+	// set goal points to 0
 	flood[3][2] = 0;
 	flood[4][2] = 0;
 	flood[3][3] = 0;
 	flood[4][3] = 0;
 
-	Queue queue;
+	Queue queue; // creates a queue of points to look at
 	Queue_init(&queue);
 
-	// append goal position
+	// append goal positions to queue
 	append(&queue, 3, 2);
 	append(&queue, 4, 2);
 	append(&queue, 3, 3);
 	append(&queue, 4, 3);
 
-	while (!isEmpty(&queue)){
+	while (!isEmpty(&queue)){ // run whilst there are available squares to search
 
-		Position current = pop(&queue);
+		Position current = pop(&queue); // get current item
 
-		int cx = current.x;
+		int cx = current.x; // x and y of item
 		int cy = current.y;
 
-		int current_val = flood[cx][cy];
-		uint8_t current_walls = map[cx][cy];
+		int current_val = flood[cx][cy]; // flood value of item
+		uint8_t current_walls = map[cx][cy]; // walls around item
 
-		for(int i = 0; i < 4; i++){
+		for(int i = 0; i < 4; i++){ // for each direction
 
-			if (!(current_walls & direction[i])){
-			//if (current_walls >> (i*2)&0b11){
-				int *dir = dir_lookup[i];
+			if (!(current_walls & direction[i])){ // if no wall
+				int *dir = dir_lookup[i]; // get delta for this dir
 
-				int nx = cx + dir[0];
+				int nx = cx + dir[0]; // get new positions
 				int ny = cy + dir[1];
 
 				// bounds check
 				if(nx >= 0 && nx < map_w && ny >= 0 && ny < map_h){
 
-					if(flood[nx][ny] == -1){
+					if(flood[nx][ny] == -1){ // set value to 1+ current if its unset
 						flood[nx][ny] = current_val + 1;
-						append(&queue, nx, ny);
+						append(&queue, nx, ny); // add this spot to
 					}
 				}
 			}
