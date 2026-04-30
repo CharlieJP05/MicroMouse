423b6ae2de2afcb58d8d1708d39a1ee1de637444
Charlie Parker <charliejparker05@gmail.com>
2026-04-30 14:50:07 +0100

    oml code innt

--- MicroMouse/Core/Src/Structs.c
+++ MicroMouse/Core/Src/Structs.c
@@ -1,8 +1,4 @@
 #include "Structs.h"
 
-typedef struct  
-{
-    float x;
-    float y;
-}vector; // this is a vector structure to hold x and y for variables
+ // this is a vector structure to hold x and y for variables
 
